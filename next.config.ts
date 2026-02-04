cae491f7118e8cd6e6a69097c68fdecba1374b30
Raven Caguioa <jerwincah4712@gmail.com>
2026-02-04 04:01:58 +0000

    image-loader.ts:74  Uncaught Error: Invalid src prop (https://gateway.pi

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'gateway.pinata.cloud',
+        port: '',
+        pathname: '/ipfs/**',
+      },
     ],
   },
 };
